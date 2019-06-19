fa8408671a7fcb8fc66f753b91d5f2075b191d18
henry <siauhenry@gmail.com>
2019-06-19 15:31:54 -0700

    add organizer

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -80,6 +80,7 @@ export default class helpers {
           "SUMMARY:" + event.title,
           "DESCRIPTION:" + event.description,
           "LOCATION:" + event.location,
+          "ORGANIZER;CN=" + event.organizer + " Reminder:MAILTO:" + event.mailTo,
           "END:VEVENT",
           "END:VCALENDAR"
         ].join("\n");
